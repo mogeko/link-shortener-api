1cca05f7d3c4cc45f1574b1b11ffc39045d0ca21
Mogeko <zhengjunyi@live.com>
2022-04-21 04:13:22 +0200

    fix(db): update migrations

--- priv/repo/migrations/20220421013047_create_links.exs
+++ priv/repo/migrations/20220421013047_create_links.exs
@@ -2,8 +2,13 @@ defmodule ShortenApi.Repo.Migrations.CreateLinks do
   use Ecto.Migration
 
   def change do
-    create table(:links) do
-      add :link, :string, null: false
+    create table(:links, primary_key: false) do
+      add :hash, :string, primary_key: true, null: false
+      add :url, :string, null: false
+
+      timestamps()
     end
+
+    create unique_index(:links, [:url])
   end
 end
